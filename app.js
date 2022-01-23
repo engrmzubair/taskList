1d79aa74fc3926404e6d43ffc0b530298964906a
engrmzubair <mz3970884@gmail.com>
2022-01-23 21:53:39 +0500

    deleteTask and filterTasks function added

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 //define UI vars
 const form = document.querySelector('#task-form');
 const taskList = document.querySelector('.collection');
-const clearBtn = document.querySelector('.clear-tasks');
+const clearBtn = document.querySelector('.clear-task');
 const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
-
 
 //loadEventsListeners
 loadEventListeners();
@@ -13,13 +12,18 @@ loadEventListeners();
 function loadEventListeners() {
   //add task event
   form.addEventListener('submit', addTask);
+  //Remove task
+  taskList.addEventListener('click', removeTask);
+  //clear all tasks
+  clearBtn.addEventListener('click', clearTasks);
+  //filter tasks
+  filter.addEventListener('keyup', filterTask)
 }
 
 //add task
 function addTask(e) {
   if (taskInput.value === '')
     return alert('Add a Task');
-
   //create Element from scratch
   const li = document.createElement('li');
   //add class
@@ -36,13 +40,30 @@ function addTask(e) {
   li.appendChild(link);
   //append li to ul
   taskList.appendChild(li);
-  alert('Task Added');
+  // alert('Task Added');
   //clear input
   taskInput.value = '';
 
-  console.log(li);
-
-
-
   e.preventDefault();
+}
+//remove task
+function removeTask(e) {
+  if (e.target.parentElement.classList.contains('delete-item'))
+    if (confirm('Are You Sure?'))
+      e.target.parentElement.parentElement.remove();
+}
+//clear all tasks 
+function clearTasks() {
+  if (confirm('Are You Sure?'))
+    while (taskList.firstChild)
+      taskList.removeChild(taskList.firstChild);
+}
+function filterTask(e) {
+  const text = e.target.value.toLowerCase();
+  document.querySelectorAll('.collection-item').forEach(task => {
+    const item = task.firstChild.textContent;
+    if (item.toLowerCase().indexOf(text) != -1)
+      return task.style.display = 'block';
+    task.style.display = 'none';
+  })
 }
